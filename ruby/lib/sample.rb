cc0474550d51f7f92b3e448d387c1ba8b8d147b6
Sam Jones <sam@samljones.com>
2016-06-13 22:45:25 -0400

    Ruby Setup

---
+++ ruby/lib/sample.rb
@@ -0,0 +1,5 @@
+class Sample
+  def self.is_tdd_fun
+    true
+  end
+end
---
+++ ruby/spec/sample_spec.rb
@@ -0,0 +1,7 @@
+require 'sample'
+
+describe "TDD" do
+  it "should let me design my code" do
+    expect(Sample.is_tdd_fun).to be_truthy
+  end
+end
